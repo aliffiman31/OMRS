6d13fded535df3c3d9b364f25eb50e45d62cb1ba
kiki <aliff.iman11@yahoo.com>
2023-06-01 01:21:11 +0800

    module5 update

--- OMRS/app/ApplicationLayer/ApplicantView/module5/mohon.js
+++ OMRS/app/ApplicationLayer/ApplicantView/module5/mohon.js
@@ -1,24 +1,64 @@
-// // JavaScript code
-// document.addEventListener("DOMContentLoaded", function() {
-//     const sections = document.querySelectorAll("section");
-//     const navLinks = document.querySelectorAll("nav a");
-//     const contentContainer = document.querySelector(".content");
+var sections = document.querySelectorAll('.content section');
+        var navLinks = document.querySelectorAll('nav ul li a');
+        var backButton = document.getElementById('backButton');
+        var nextButton = document.getElementById('nextButton');
+        var currentSectionIndex = 0;
 
-//     function showSection(id) {
-//         sections.forEach(function(section) {
-//             section.style.display = "none";
-//         });
-//         document.getElementById(id).style.display = "block";
-//         contentContainer.innerHTML = document.getElementById(id).innerHTML;
-//     }
+        function showCurrentSection() {
+            for (var i = 0; i < sections.length; i++) {
+                sections[i].classList.remove('active');
+                navLinks[i].classList.remove('active');
+            }
 
-//     navLinks.forEach(function(navLink) {
-//         navLink.addEventListener("click", function(e) {
-//             e.preventDefault();
-//             const targetId = this.getAttribute("href").substring(1);
-//             showSection(targetId);
-//         });
-//     });
+            sections[currentSectionIndex].classList.add('active');
+            navLinks[currentSectionIndex].classList.add('active');
 
-//     showSection("suami");
-// });
+            if (currentSectionIndex === 0) {
+                backButton.disabled = true;
+            } else {
+                backButton.disabled = false;
+            }
+
+            if (currentSectionIndex === sections.length - 1) {
+                nextButton.disabled = true;
+            } else {
+                nextButton.disabled = false;
+            }
+        }
+
+        function navigateToSection(sectionId) {
+            var sectionIndex = Array.from(sections).findIndex(function (section) {
+                return section.id === sectionId;
+            });
+
+            if (sectionIndex !== -1) {
+                currentSectionIndex = sectionIndex;
+                showCurrentSection();
+            }
+        }
+
+        function navigateToNextSection() {
+            if (currentSectionIndex < sections.length - 1) {
+                currentSectionIndex++;
+                showCurrentSection();
+            }
+        }
+
+        function navigateToPreviousSection() {
+            if (currentSectionIndex > 0) {
+                currentSectionIndex--;
+                showCurrentSection();
+            }
+        }
+
+        // Check URL hash on page load
+        var initialSectionId = window.location.hash.slice(1);
+        var initialIndex = Array.from(sections).findIndex(function (section) {
+            return section.id === initialSectionId;
+        });
+
+        if (initialIndex !== -1) {
+            currentSectionIndex = initialIndex;
+        }
+
+        showCurrentSection();
--- OMRS/app/ApplicationLayer/ApplicantView/module5/mohon.php
+++ OMRS/app/ApplicationLayer/ApplicantView/module5/mohon.php
@@ -6,34 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Syarat Kelayakan</title>
     <link rel="stylesheet" href="mohon.css">
-    <style>
-        /* Updated CSS */
-        nav ul {
-            list-style: none;
-            padding: 0;
-            display: inline-block;
-        }
-        nav ul li {
-            display: inline-block;
-            margin-right: 10px;
-        }
-        nav ul li a {
-            text-decoration: none;
-            color: #333;
-            padding: 5px;
-            border-radius: 5px;
-        }
-        nav ul li a.active {
-            background: black;
-            color: white;
-        }
-        .content section {
-            display: none;
-        }
-        .content section.active {
-            display: block;
-        }
-    </style>
+
 </head>
 <body>
     <div>
@@ -85,71 +58,5 @@
     </div>
 
     <script src="mohon.js"></script>
-    <script>
-        var sections = document.querySelectorAll('.content section');
-        var navLinks = document.querySelectorAll('nav ul li a');
-        var backButton = document.getElementById('backButton');
-        var nextButton = document.getElementById('nextButton');
-        var currentSectionIndex = 0;
-
-        function showCurrentSection() {
-            for (var i = 0; i < sections.length; i++) {
-                sections[i].classList.remove('active');
-                navLinks[i].classList.remove('active');
-            }
-
-            sections[currentSectionIndex].classList.add('active');
-            navLinks[currentSectionIndex].classList.add('active');
-
-            if (currentSectionIndex === 0) {
-                backButton.disabled = true;
-            } else {
-                backButton.disabled = false;
-            }
-
-            if (currentSectionIndex === sections.length - 1) {
-                nextButton.disabled = true;
-            } else {
-                nextButton.disabled = false;
-            }
-        }
-
-        function navigateToSection(sectionId) {
-            var sectionIndex = Array.from(sections).findIndex(function (section) {
-                return section.id === sectionId;
-            });
-
-            if (sectionIndex !== -1) {
-                currentSectionIndex = sectionIndex;
-                showCurrentSection();
-            }
-        }
-
-        function navigateToNextSection() {
-            if (currentSectionIndex < sections.length - 1) {
-                currentSectionIndex++;
-                showCurrentSection();
-            }
-        }
-
-        function navigateToPreviousSection() {
-            if (currentSectionIndex > 0) {
-                currentSectionIndex--;
-                showCurrentSection();
-            }
-        }
-
-        // Check URL hash on page load
-        var initialSectionId = window.location.hash.slice(1);
-        var initialIndex = Array.from(sections).findIndex(function (section) {
-            return section.id === initialSectionId;
-        });
-
-        if (initialIndex !== -1) {
-            currentSectionIndex = initialIndex;
-        }
-
-        showCurrentSection();
-    </script>
 </body>
 </html>
