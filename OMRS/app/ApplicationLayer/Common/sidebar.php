e7d7ae4d3b4e0295623cda3a0092aaa3eeab612c
kiki <aliff.iman11@yahoo.com>
2023-06-01 17:11:43 +0800

    sidebar change

--- OMRS/app/ApplicationLayer/Common/sidebar.php
+++ OMRS/app/ApplicationLayer/Common/sidebar.php
@@ -314,53 +314,69 @@ main {
 
     <!-- Sidebar -->
   <div id="collapsible" class="flex-shrink-0 p-3" style="width: 280px; border: 1px solid black;">
-    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
-      <svg class="bi pe-none me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-5 fw-semibold">Collapsible</span>
-    </a>
     <ul class="list-unstyled ps-0">
+
+    <!-- MODULE 1 MENU -->
       <li class="mb-1">
         <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-          Home
+          Profile
         </button>
         <div class="collapse show" id="home-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Overview</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Updates</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Reports</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">View Account</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Updates Account</a></li>
           </ul>
         </div>
       </li>
+
+      <!-- MODULE 2 MENU -->
       <li class="mb-1">
         <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-          Dashboard
+          Permohonan Kursus Kahwin
         </button>
         <div class="collapse" id="dashboard-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Overview</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Weekly</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Monthly</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Annually</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option1</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option2</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option3</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option4</a></li>
           </ul>
         </div>
       </li>
+
+      <!-- MODULE 2 MENU -->
       <li class="mb-1">
         <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-          Orders
+          Permohonan Berkahwin
         </button>
         <div class="collapse" id="orders-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">New</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Processed</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Shipped</a></li>
-            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Returned</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option1</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option2</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option3</a></li>
+            <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">option4</a></li>
           </ul>
         </div>
       </li>
-      <li class="border-top my-3"></li>
+      <!-- <li class="border-top my-3"></li> -->
+      <!-- MODULE 3 MENU -->
       <li class="mb-1">
         <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          Account
+          Pendaftaran Perkahwinan
+        </button>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">New...</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Profile</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Settings</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
+          </ul>
+        </div>
+      </li>
+      <!-- MODULE 4 MENU -->
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+          Pendaftaran Perkahwinan
         </button>
         <div class="collapse" id="account-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
