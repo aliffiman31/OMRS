9bd0d1e11498fdbb558bf97f259cd4778a2798c0
Liviya <liviyashinimahendramoorthi@gmail.com>
2023-06-06 16:30:14 +0800

    test marriage type

---
+++ app/ApplicationLayer/ApplicantView/module3/marriagetype.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+        <!-- Header -->
+        <?php
+        include_once('../../Common/header.php');
+        ?>
+
+
+        <section>
+
+            <div>
+                <?php include_once('../../Common/sidebar.php');  ?>
+            </div>
+
+            <div class="content">
+        
+        <div class="box">
+            <h1>~PERMOHONAN PERKAHWINAN~</h1>
+            </div>
+        <li class="smallbox"><a>JENIS PERKAHWINAN</a></li>
+        <ol class="optionnikah">
+            <li class="nikah"><a href="nikah tanpa kebenaran.html">Perkahwinan Tanpa Kebenaran</a></li>
+            <li class="nikah"><a href="nikah sukarela.html">Perkahwinan Sukarela</a></li>
+        </ol>
+        <li class="smallbox"><a href="print marriage cert.html">SIJIL DAN KAD NIKAH</a></li>
+    </form>
+    </div>
+    
+</body>
+</html>
