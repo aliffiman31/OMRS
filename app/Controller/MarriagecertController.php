57e5bc8b7f7480948e43e3452fe5e295ea707a55
wafi2002 <wafi2002ramli@gmail.com>
2024-05-16 00:58:31 +0800

    MM

--- app/Controller/MarriagecertController.php
+++ app/Controller/MarriagecertController.php
@@ -4,7 +4,6 @@
      
     public function Insertform($marriage_cert_ID, $VoluntaryMarriage_ID, $UnauthorizedMarriage_ID, $Fee_id, $applied_date, $payment_proof_status)
     {
-<<<<<<< HEAD
         //$this->Module3Repository->($marriage_cert_ID, $VoluntaryMarriage_ID, $UnauthorizedMarriage_ID, $Fee_id, $applied_date, $payment_proof_status);
         $this->module3Repository->methodName($marriage_cert_ID, $VoluntaryMarriage_ID, $UnauthorizedMarriage_ID, $Fee_id, $applied_date, $payment_proof_status);
 
@@ -18,13 +17,5 @@
     // {
     // //    return $this->Module3Repository->getapplystatusData();
     // }
-=======
-       // $this->Module3Repository->($marriage_cert_ID, $VoluntaryMarriage_ID, $UnauthorizedMarriage_ID, $Fee_id, $applied_date, $payment_proof_status);
-    }
-    public function getapplystatusData()
-    {
-    //    return $this->Module3Repository->getapplystatusData();
-    }
->>>>>>> 04c3a3fb3292fbbd98e073798e2e55a91a652849
 }
 ?>
